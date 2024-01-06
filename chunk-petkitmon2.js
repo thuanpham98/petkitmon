0a9b3fc1a7efdd577716252eeccbc94bb9ea63f4
thuanpm <thuanpm@thuanpms-mbp.0.1.168.192.in-addr.arpa>
2024-01-07 02:45:47 +0700

    Updates

--- chunk-petkitmon2.js
+++ chunk-petkitmon2.js
@@ -1,2 +1,2 @@
-import{j as t,O as o}from"./chunk-petkitmon.js";function r(){return t.jsx("div",{style:{width:"100%",height:"100%",minHeight:"100vh",overflow:"auto",backgroundColor:"#FAFAFA"},children:t.jsx(o,{})})}export{r as default};
+import{j as t,O as o}from"./chunk-petkitmon.js";function r(){return t.jsx("div",{style:{width:"100%",height:"100%",minHeight:"100vh",overflow:"auto",backgroundColor:"#fafafa"},children:t.jsx(o,{})})}export{r as default};
 //# sourceMappingURL=chunk-petkitmon2.js.map
